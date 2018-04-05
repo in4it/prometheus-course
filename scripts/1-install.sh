0a6ad18601c33495f49575151a854d571e86869e
Edward Viaene <ward.viaene@gmail.com>
2018-04-05 13:12:06 +0000

    installation script

---
+++ scripts/1-install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+PROMETHEUS_VERSION="2.2.1"
+wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
+tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
+cd prometheus-2.2.1.linux-amd64/
+# if you just want to start prometheus as root
+#./prometheus --config.file=prometheus.yml
+
+# create users
+useradd --no-create-home --shell /bin/false prometheus
+useradd --no-create-home --shell /bin/false node_exporter
+
+# create directories
+mkdir -p /etc/prometheus
+mkdir -p /var/lib/prometheus
+
+# set ownership
+chown prometheus:prometheus /etc/prometheus
+chown prometheus:prometheus /var/lib/prometheus
+
+# copy binaries
+cp prometheus /usr/local/bin/
+cp promtool /usr/local/bin/
+
+chown prometheus:prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/promtool
+
+# copy config
+cp -r consoles /etc/prometheus
+cp -r console_libraries /etc/prometheus
+cp prometheus.yml /etc/prometheus/prometheus.yml
+
+chown -R prometheus:prometheus /etc/prometheus/consoles
+chown -R prometheus:prometheus /etc/prometheus/console_libraries
+
+# setup systemd
+echo '[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/prometheus.service
+
+systemctl daemon-reload
+systemctl enable prometheus
