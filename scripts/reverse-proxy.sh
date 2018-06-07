7eb5ff2037800f01da4e6b77548177443f4b9e2c
Edward Viaene <ward.viaene@gmail.com>
2018-06-07 08:36:42 +0000

    reverse proxy

---
+++ scripts/reverse-proxy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# run script as root or with sudo
+
+# install nginx and openssl
+apt -y install nginx openssl apache2-utils
+
+# generate ssl certificate (host prometheus.example.com)
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=prometheus.example.com' -nodes
+mv key.pem /etc/ssl/private/nginx.pem
+chmod 600 /etc/ssl/private/nginx.pem
+mv cert.pem /etc/ssl/certs/nginx.pem
+
+echo 'server {
+  listen 443;
+  ssl    on;
+  ssl_certificate /etc/ssl/certs/nginx.pem;
+  ssl_certificate_key /etc/ssl/private/nginx.pem;
+  location / {
+    proxy_pass http://localhost:9090/;
+    auth_basic "Prometheus";
+    auth_basic_user_file /etc/nginx/.htpasswd;
+  }
+}' > /etc/nginx/sites-enabled/prometheus
+
+systemctl enable nginx
+systemctl restart nginx
+
+EXTERNAL_IP=$(curl -s ifconfig.co)
+echo "Reverse proxy enabled on http://${EXTERNAL_IP}:19090"
